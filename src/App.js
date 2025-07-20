768d0cbe10e62b733e26ea89ddb1c3c412138b6d
gokulkrishnaaaa <gokulkrishna1301@gmail.com>
2025-07-20 23:13:22 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div>
-      <h3>Netflix Clone</h3>
+    <div className="bg-blue-500 text-white p-4 text-2xl">
+      <h3>Netflix Clone, Tailwind is working..</h3>
     </div>
   );
 }
